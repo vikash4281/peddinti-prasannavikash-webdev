ab0b2161ad963261222a08abaddd8e706c839de7
vikash4281 <peddinti.p@husky.neu.edu>
2017-03-02 14:39:40 -0500

    final update

--- public/assignment/directives/sortable.js
+++ public/assignment/directives/sortable.js
@@ -1,13 +1,13 @@
 (function () {
-    console.log("sortable loaded");
+
     angular
         .module('directives', [])
         .directive('sortable', sortableDir);
 
     function sortableDir() {
-        console.log("in sortable");
+
         function linkFunc(scope, element) {
-            console.log("Sortable Dir");
+    
             element
                 .sortable({
                     axis: 'y',
